d2d5ed0a5ff87f17b030191576e6a67e5bf39a17
Russell Warren <2751728+rwarren@users.noreply.github.com>
2021-12-10 18:00:53 -0500

    residual cleanup from huge repo rebase

--- install.sh
+++ install.sh
@@ -1,4 +1,20 @@
 #!/bin/bash
+
+# Check current HDDcoin SSL version to prevent update on old SSL
+if [ -e ../.hddcoin/mainnet/config/ssl/ca/hddcoin_ca.crt ]; then
+	HDDCOIN_SSL_SERIAL=$(openssl x509 -noout -in ../.hddcoin/mainnet/config/ssl/ca/hddcoin_ca.crt -serial)
+	if [ $HDDCOIN_SSL_SERIAL = "serial=5C8A71239328650EB9FEF85CEC32BF779CA6A0C5" ]; then 
+		echo ""
+		echo "WARNING:"
+		echo "Old version of HDDcoin Blockchain SSL has been detected."
+		echo "Please visit https://hddcoin.org/sslupdate/ for further instructions."
+		echo ""
+		echo "Exiting installer..."
+		echo ""
+		exit 1
+	fi
+fi
+
 set -e
 UBUNTU=false
 DEBIAN=false
@@ -181,9 +197,8 @@ fi
 
 echo "HDDcoin blockchain install.sh complete."															  
 echo ""
-echo "HDDcoin blockchain install.sh complete."
-echo "For assistance join us on Keybase in the #support chat channel:"
-echo "https://keybase.io/team/hddcoin_network.public"
+echo "Visit our Website to learn more about HDDcoin:"
+echo "https://hddcoin.org"
 echo ""
 echo "Try the Quick Start Guide to running hddcoin-blockchain:"
 echo "https://github.com/HDDcoin-Network/hddcoin-blockchain/wiki/Quick-Start-Guide"
