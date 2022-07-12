09c772a0e5d235619cba4792741c6848bb4e2471
Felix Brucker <felix@felixbrucker.com>
2022-07-12 18:21:03 +0200

    Bump og version to 1.2.0

--- setup.py
+++ setup.py
@@ -122,7 +122,7 @@ kwargs = dict(
         "hddcoin.ssl": ["hddcoin_ca.crt", "hddcoin_ca.key", "dst_root_ca.pem"],
         "mozilla-ca": ["cacert.pem"],
     },
-    version="2.0.2+og-1.1.1",
+    version="2.0.2+og-1.2.0",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
     zip_safe=False,
